022177bcc14e9cb6f771745afbab5e19820d72d1
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2022-07-27 15:35:20 -0300

    Criando função que move personagens

--- index.js
+++ index.js
@@ -22,12 +22,16 @@ carrot.id = "carrot";
 carrot.className="animals";
 carrot.src = "./images/carrot.png"
 
+let cont = 0;
+
 start.addEventListener('click', () =>{
+
     let startScreen = document.querySelector("#startScreen");
 
     startScreen.style.display = "none";
 
     boatDiv.style.justifyContent = 'flex-start';
+    cont = 0;
 
     sheep.style.position= "static";
     wolf.style.position= "static";
@@ -36,4 +40,94 @@ start.addEventListener('click', () =>{
     leftSide.appendChild(wolf);
     leftSide.appendChild(carrot);
     
+})
+
+sheep.addEventListener('click', () => {
+
+    if(boatDiv.style.justifyContent == "flex-start"){
+        if(leftSide.querySelector("#sheep") != null && cont == 0){
+            leftSide.removeChild(sheep);
+            sheep.style.position="absolute";
+            boatDiv.appendChild(sheep);
+            cont++;
+        }else if(boatDiv.querySelector("#sheep")!=null){
+            boatDiv.removeChild(sheep);
+            sheep.style.position= "static";
+            leftSide.appendChild(sheep);
+            cont--;
+        }
+    }else{
+        if(rightSide.querySelector("#sheep") != null && cont == 0){
+            rightSide.removeChild(sheep);
+            sheep.style.position="absolute";
+            boatDiv.appendChild(sheep);
+            cont++;
+        }else if(boatDiv.querySelector("#sheep")!=null){
+            boatDiv.removeChild(sheep);
+            sheep.style.position= "static";
+            rightSide.appendChild(sheep);
+            cont--;
+        }
+    }
+
+})
+
+wolf.addEventListener('click', () => {
+
+    if(boatDiv.style.justifyContent == "flex-start"){
+        if(leftSide.querySelector("#wolf") != null && cont == 0){
+            leftSide.removeChild(wolf);
+            wolf.style.position="absolute";
+            boatDiv.appendChild(wolf);
+            cont++;
+        }else if(boatDiv.querySelector("#wolf")!=null){
+            boatDiv.removeChild(wolf);
+            wolf.style.position= "static";
+            leftSide.appendChild(wolf);
+            cont--;
+        }
+    }else{
+        if(rightSide.querySelector("#wolf") != null && cont == 0){
+            rightSide.removeChild(wolf);
+            wolf.style.position="absolute";
+            boatDiv.appendChild(wolf);
+            cont ++;
+        }else if(boatDiv.querySelector("#wolf")!=null){
+            boatDiv.removeChild(wolf);
+            wolf.style.position= "static";
+            rightSide.appendChild(wolf);
+            cont--;
+        }
+    }
+
+})
+
+carrot.addEventListener('click', () => {
+
+    if(boatDiv.style.justifyContent == "flex-start"){
+        if(leftSide.querySelector("#carrot") != null && cont == 0){
+            leftSide.removeChild(carrot);
+            carrot.style.position="absolute";
+            boatDiv.appendChild(carrot);
+            cont++;
+        }else if(boatDiv.querySelector("#carrot")!=null){
+            boatDiv.removeChild(carrot);
+            carrot.style.position= "static";
+            leftSide.appendChild(carrot);
+            cont--;
+        }
+    }else{
+        if(rightSide.querySelector("#carrot") != null && cont == 0){
+            rightSide.removeChild(carrot);
+            carrot.style.position="absolute";
+            boatDiv.appendChild(carrot);
+            cont++;
+        }else if(boatDiv.querySelector("#carrot")!=null){
+            boatDiv.removeChild(carrot);
+            carrot.style.position= "static";
+            rightSide.appendChild(carrot);
+            cont--;
+        }
+    }
+
 })
